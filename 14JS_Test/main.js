021394786e1442bd90aee6433f49e25b82f375f6
Junbro0708 <qkrwns708@soongsil.ac.kr>
2022-03-18 15:09:40 +0900

    JS 기본

---
+++ 14JS_Test/main.js
@@ -0,0 +1,30 @@
+let myName = "Junbro";
+let email = 'aktm708@naver.com';
+let hello_ = `Hello ${myName}?!`;
+
+console.log(myName);
+console.log(email);
+console.log(hello_);
+
+let user = {
+  name: 'Junbro',
+  age: 26,
+  isValid: true
+};
+console.log(user.name);
+console.log(user.age);
+
+function hello(){
+  console.log('Hello~');
+}
+
+const junbro = {
+  name:'Junbro',
+  age: 26,
+  getName: function(){
+    return this.name;
+  }
+};
+const hisName = junbro.getName();
+console.log(hisName);
+console.log(junbro.getName());
