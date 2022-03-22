7264a6d3fb6b4a9e88f4a05ff79bc6bfaaabf046
Junbro0708 <qkrwns708@soongsil.ac.kr>
2022-03-22 14:17:55 +0900

    스타벅스 전역배지

--- 15Starbucks/js/main.js
+++ 15Starbucks/js/main.js
@@ -14,3 +14,23 @@ searchInputEL.addEventListener('blur', function(){
   searchEL.classList.remove('focused');
   searchInputEL.setAttribute('placeholder', '');
 });
+
+
+const badgeEL = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+  console.log('scroll!', );
+  if(window.scrollY > 500){
+    gsap.to(badgeEL, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+    //gsap.to(요소, 지속시간, 옵션);
+  }else{
+    gsap.to(badgeEL, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
+// ._thorottle(함수, 몇 초에 한번 실행되는지 300은 0.3초)
