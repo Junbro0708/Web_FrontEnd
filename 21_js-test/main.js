824fda00399c9718fd5d4dd64028c6372f7af784
Junbro0708 <aktm708@naver.com>
2022-04-20 16:06:49 +0900

    js 시작

---
+++ 21_js-test/main.js
@@ -0,0 +1,65 @@
+// import GetType from './GetType'
+import abc from './getRandom'
+
+// console.log(typeof "Hello World!")
+// console.log(typeof 123)
+// console.log(typeof true)
+// console.log(typeof undefined)
+// console.log(typeof null)
+// console.log(typeof {})
+// console.log(typeof [])
+
+// console.log(GetType("Hello World!"))
+// console.log(GetType(123))
+// console.log(GetType(true))
+// console.log(GetType(undefined))
+// console.log(GetType(null))
+// console.log(GetType({}))
+// console.log(GetType([]))
+
+// ---------------------------------
+
+// console.log(1 + 2)
+// console.log(5 - 7)
+// console.log(3 * 4)
+// console.log(10 / 2)
+// console.log(7 % 3)
+
+// const a = 2 // = 는 할당 연산자
+// a += 1
+// console.log(a)
+
+// ---------------------------------
+
+// const a = 1
+// const b = "1"
+// const c = 3
+
+// console.log(a === b)
+// console.log(a !== b)
+// console.log(a == b)
+// console.log(a != b)
+
+// console.log(a < c)
+// console.log(a > c)
+// console.log(a <= c)
+// console.log(a >= c)
+
+// function isEqule(x, y){
+//   return x == y
+// }
+
+// console.log(isEqule(a, b))
+
+// ---------------------------------
+
+const a = 1 < 2
+
+if(a) {
+  console.log('참')
+}else{
+  console.log('거짓')
+}
+
+console.log(a ? true : false)
+console.log(abc())
