6a7278525dbabab7f5b48ef25a94c62bb1e82ddf
Junbro0708 <aktm708@naver.com>
2022-04-22 08:35:33 +0900

    switch문 else if문 비교

--- 21_js-test/main.js
+++ 21_js-test/main.js
@@ -82,3 +82,13 @@ switch (a) {
     console.log('rest...')
     break
 }
+
+if(a === 0){
+  console.log('a is 0')
+}else if(a === 2){
+  console.log('a is 2')
+}else if(a === 4){
+  console.log('a is 4')
+}else{
+  console.log('rest...')
+}
