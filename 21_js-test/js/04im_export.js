48ddaaa0d23111942f1d5a13b031883787d4538d
Junbro0708 <aktm708@naver.com>
2022-05-05 01:02:05 +0900

    js export named default

--- 21_js-test/js/04im_export.js
+++ 21_js-test/js/04im_export.js
@@ -1,7 +1,10 @@
 import _ from 'lodash'
 import getType from './GetType'
-import { rando } from './getRandom'
+import { rando, user as junbro } from './getRandom'
+// import * as R from './getRandom' // 한번에 가져오기
 
 console.log(_.camelCase('the hello world'))
 console.log(getType([1, 2, 3]))
 console.log(rando(), rando())
+console.log(junbro)
+// console.log(R)
--- 21_js-test/js/getRandom.js
+++ 21_js-test/js/getRandom.js
@@ -1,3 +1,9 @@
 export function rando(){
   return Math.floor(Math.random() * 10)
 }
+export const user = {
+  name: 'Junbro',
+  age: 26
+}
+
+export default 123
