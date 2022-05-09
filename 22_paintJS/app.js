21f0f2fbe9114e9878154442b3e1bce347669713
Junbro0708 <aktm708@naver.com>
2022-05-09 13:52:05 +0900

    그림판 JS

--- 22_paintJS/app.js
+++ 22_paintJS/app.js
@@ -1,4 +1,10 @@
 const canvas = document.getElementById("jsCanvas")
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700
+canvas.height = 700
+ctx.strokeStyle = "#2c2c2c"
+ctx.lineWidth = 2.5
 
 let isPainting = false
 
@@ -6,23 +12,29 @@ function stopPainting(){
   isPainting = false
 }
 
+function startPainting(){
+  isPainting = true
+}
+
 function onMouseMove(event){
   const x = event.offsetX
   const y = event.offsetY
-  console.log(x, y)
+  if(!isPainting){
+    ctx.beginPath();
+    ctx.moveTo(x, y)
+  } else{
+    ctx.lineTo(x, y)
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(event){
   isPainting = true
 }
 
-function onMouseUp(event){
-  stopPainting()
-}
-
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove)
-  canvas.addEventListener("mousedown", onMouseDown)
-  canvas.addEventListener("mouseup", onMouseUp)
+  canvas.addEventListener("mousedown", startPainting)
+  canvas.addEventListener("mouseup", stopPainting)
   canvas.addEventListener("mouseleave", stopPainting)
 }
