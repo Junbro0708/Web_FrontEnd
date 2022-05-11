b5d1634bef9b1f02e08f8cb29df95f3f39289720
Junbro0708 <aktm708@naver.com>
2022-05-11 16:36:31 +0900

    Update app.js

--- 22_paintJS/app.js
+++ 22_paintJS/app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById("jsCanvas")
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor")
+const range = document.getElementById("jsRange")
+const mode = document.getElementById("jsMode")
 
 canvas.width = 700
 canvas.height = 700
@@ -8,6 +10,7 @@ ctx.strokeStyle = "#2c2c2c"
 ctx.lineWidth = 2.5
 
 let isPainting = false
+let isFilling = true
 
 function stopPainting(){
   isPainting = false
@@ -34,6 +37,21 @@ function changeColor(event){
   ctx.strokeStyle = color
 }
 
+function handleRangeChange(event){
+  const size = event.target.value
+  ctx.lineWidth = size
+}
+
+function handleModeClick(){
+    if(isFilling){
+      isFilling = false
+      mode.innerText = "PAINT"
+    }else{
+      isFilling = true
+      mode.innerText = "FILL"
+    }
+  }
+
 if(canvas){
   canvas.addEventListener("mousemove", onMouseMove)
   canvas.addEventListener("mousedown", startPainting)
@@ -43,3 +61,11 @@ if(canvas){
 
 Array.from(colors).forEach(color => 
   color.addEventListener("click", changeColor))
+
+if(range){
+  range.addEventListener("input", handleRangeChange)
+}
+
+if(mode){
+  mode.addEventListener("click", handleModeClick)
+}
