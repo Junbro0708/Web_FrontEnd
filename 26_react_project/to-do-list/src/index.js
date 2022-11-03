225749fd34a63b72b819b1d14dfa657f833c1d2e
Junbro0708 <aktm708@naver.com>
2022-11-02 19:37:46 -0700

    react

--- 26_react_project/to-do-list/src/index.js
+++ 26_react_project/to-do-list/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App2';
+import App from './App3';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <App/>
 );
 
