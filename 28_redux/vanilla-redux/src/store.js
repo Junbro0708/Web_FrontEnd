9df86164ed3a635b90d0f4c47c5c41a6f58e1621
Junbro0708 <aktm708@naver.com>
2023-01-16 11:04:50 -0800

    redux toolkit

--- 28_redux/vanilla-redux/src/store.js
+++ 28_redux/vanilla-redux/src/store.js
@@ -1,32 +1,46 @@
-import { createStore } from "redux";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// We can replace below sentences with createAction method
+// const ADD = "ADD";
+// const DELETE = "DELETE";
+// 
+// export const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text
+//   }
+// }
+// export const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id
+//   }
+// }
 
-export const addToDo = (text) => {
-  return {
-    type: ADD,
-    text
-  }
-}
-export const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id
-  }
-}
+export const addToDo = createAction("ADD");
+export const deleteToDo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-   switch(action.type){
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter(toDo => toDo.id !== action.id);
-    default:
-      return state;
-   }
-}
+console.log(addToDo(), deleteToDo())
 
-const store = createStore(reducer);
+// const reducer = (state = [], action) => {
+//    switch(action.type){
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter(toDo => toDo.id !== action.payload);
+//     default:
+//       return state;
+//    }
+// }
+
+const reducer = createReducer([], {
+  [addToDo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() })
+  },
+  [deleteToDo]: (state, action) =>
+    state.filter(toDo => toDo.id !== action.payload)
+})
+
+const store = configureStore({reducer});
 
 export default store;
